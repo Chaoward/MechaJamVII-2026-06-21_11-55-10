9934678d0fa3bfc456381bc757f12db5aaa03101
bkobei <115329058+bkobei@users.noreply.github.com>
2026-07-01 20:00:31 -0700

    Added laser sounds

--- Assets/PlayerAudio.cs
+++ Assets/PlayerAudio.cs
@@ -1,15 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerAudio : MonoBehaviour
 {
-    [SerializeField] private MechMove movement;
+    [SerializeField] private GameObject player;
+    
     [SerializeField] private AudioSource footstepAudio;
     [SerializeField] private AudioSource laserAudio;
+    [SerializeField] private AudioSource jumpAudio;
 
     private void Update()
     {
+        var movement = player.GetComponent<MechMove>();
+        var targeting = player.GetComponent<MechTargeting>();
+
+        InputAction jumpAction = InputHandler.curActionMap.FindAction("Jump");
+
         if (movement.isMoving && movement.isGrounded)
         {
             if (!footstepAudio.isPlaying)
@@ -20,5 +28,17 @@ public class PlayerAudio : MonoBehaviour
             if (footstepAudio.isPlaying)
                 footstepAudio.Stop();
         }
+
+        if (targeting.isFiring)
+        {
+            laserAudio.Play();
+        }
+
+        if (jumpAction.WasPressedThisFrame() && movement.isGrounded)
+        {
+            jumpAudio.Play();
+        }
+
+
     }
 }
